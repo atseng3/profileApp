39eed3d7de1ee31fe6b248e6a84776b26c6e9153
Albert Tseng <atseng3@gmail.com>
2014-10-01 19:24:32 -0700

    add default url options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ ProfileApp::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.default_url_options = { :host => 'profile-app.herokuapp.com' }
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
